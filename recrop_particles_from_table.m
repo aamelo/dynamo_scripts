eb9e944cdcf9e05a75c2192e18fcab5a7fb1c6b0
Arthur Melo <59894900+aamelo@users.noreply.github.com>
2020-01-28 17:39:36 -0800

    Update recrop_particles_from_table.m

--- recrop_particles_from_table.m
+++ recrop_particles_from_table.m
@@ -6,8 +6,13 @@ tomo=dread(<tomogram>);
 %Example:>> tomo=dread('bits_234_bin4_WBP.rec');
 
 %Table as "tb"
-ddb <project_name>:rt -ws tb;
+ddb <project_name>:rt -ws tb1;
 %Example:>> ddb bits234_r1:rt -ws tb;
+%Preparing table for recroping: Add columns 4 to 6 to columns 24 to 26 and center particles
+tbnew=tb1;
+tbnew(:,24:26)=tb(:,24:26) + tb(:,4:6);
+tbnew = dpktbl.centerCropPoints (tbnew);
+tb=tbnew;
 
 %targetFolder
 myData='<file_path>';
