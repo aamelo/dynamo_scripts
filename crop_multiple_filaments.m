f7befe183134862b0d428323792365189f3a57ea
Arthur Melo <59894900+aamelo@users.noreply.github.com>
2020-05-25 14:29:46 -0700

    Create crop_multiple_filaments.m

---
+++ crop_multiple_filaments.m
@@ -0,0 +1,29 @@
+%User input - Start this script on the root folder
+dcmodels <catalogue_name> -nc mfilament -ws o -gm 1 %nc --> contains the following terminology
+
+c=1; % counter for table
+for tomo = 1:length(o.models)   % loop over tomograms
+    m = o.models{tomo};  % read models
+    myTable{c}=m.grepTable(); %get table from model
+    tomogram=m.cvolume.file; %get tomogram path
+    box='180'; %define boxsize
+    Ntomo=tomogram(:,60:62); %get number of the tomogram
+    label=m.cvolume.catalogue.label; %label of the catalogue
+    myData=(['particles/' label '/bits_' Ntomo '_tube_' num2str(c) '_box_' box]); %Define output data for particles
+    dwrite(myTable{c},['table/' m.cvolume.catalogue.label '/bits_' tomogram(:,60:62) '_tube_' num2str(c) '.tbl']);
+    
+    %Cropping particles
+    dtcrop(tomogram,myTable{c},myData,box,'mw',5); 
+    
+    %update cropTable
+    myTable_cropped{c}= dread([myData, '/crop.tbl']);
+    
+    %Randomizing table along azimuth and average
+    tbRand=dynamo_table_randomize_azimuth(myTable_cropped{c}); %randomize table along azimuth
+    oaRand=daverage(myData,'t',tbRand,'fc',1,'mw',5); %average randomized particles
+    
+    %Saving files
+    dwrite(tbRand,['table/' m.cvolume.catalogue.label '/bits_' tomogram(:,60:62) '_tube_' num2str(c) '_Randomized.tbl']);
+    dwrite(oaRand.average,['template/' m.cvolume.catalogue.label '/bits_' tomogram(:,60:62) '_tube_' num2str(c) '_box_' box '.em']);
+    c=c+1;
+end
